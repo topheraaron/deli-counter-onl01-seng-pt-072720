3d1dac1b9ea3f3597f0fa30c503f73663c112cdf
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-05 03:58:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,40 +1,10 @@
-#katz_deli = []
-#def line(katz_deli)
-  #if katz_deli[0] == nil
-  #    puts "The line is currently empty."
-  #end
-#end
-
-#def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-#end
-
 katz_deli = []
-
 def line(katz_deli)
-  line_positions = [];
-  if katz_deli.empty?
+  if katz_deli[0] == nil
     puts "The line is currently empty."
-  else
-    number = 1
-    katz_deli.each do |customer|
-      line_positions << "#{number}. #{customer}"
-      number += 1
-    end
-    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
-end
-
-def now_serving(katz_deli)
-  if katz_deli.empty?
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
-  end
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
 end
